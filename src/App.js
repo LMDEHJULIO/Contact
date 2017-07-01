c6e6c9ec95f592eb19c9d6fc167a7a27f318e63c
Julio Rodriguez <juliodevelops@gmail.com>
2017-07-01 15:58:09 -0400

    Add Route Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './listContacts.js'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
@@ -6,7 +7,6 @@ import * as ContactsAPI from './utils/ContactsAPI'
 class App extends Component {
 
   state = {
-    screen: 'list', //list, create
     contacts:[]
   }
 
@@ -31,22 +31,15 @@ class App extends Component {
   render(){
     return (
       <div className="app">
-        {this.state.screen === 'list' && (
-          <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-          onNavigate={() => {
-            this.setState({screen: 'create'})
-          }}
-          />
-        )}
-        {this.state.screen === 'create' &&(
-          <CreateContact />
-        )}
-      </div>
-    )
+          <Route exact path="/" render= {()=>(
+            <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}/>
+        )}/>
 
-  }
+        <Route path="/create" component= {CreateContact}/>
+      </div>
+    )}
 }
 
 export default App;
